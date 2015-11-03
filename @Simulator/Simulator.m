54df47f91b87cd2e59727fbce129d5bfa4d2ca46
ajnedorezov <ajnedorezov@gmail.com>
2015-11-03 15:30:17 -0500

    Achieved Realistic Scenario
    Achieved realistic scenario, now it's time to start inserting the
    vehicle dynamics, etc.

--- @Simulator/Simulator.m
+++ @Simulator/Simulator.m
@@ -18,9 +18,9 @@ classdef Simulator < handle
 
         RoadParams = struct('numLanes', 3,...
                             'laneWidth', 3,... %m
-                            'laneMarkerWidth', 0.15/2,... %m (6in)
-                            'roadColor', [0.3 0.3 0.3],...
-                            'laneColor', [1 0.95 0]);
+                            'laneMarkerWidth', 0.127,... %m (6in)
+                            'roadColor', [0.1 0.1 0.1],...
+                            'laneColor', [1 1 1]);
 
         DriverParams = struct('lookAheadDistance', 0.5 * 1609.34); %m
         
@@ -32,16 +32,16 @@ classdef Simulator < handle
         CameraParams = struct('height', 1);
     end
     
-    
     methods
-        
         function self = Simulator(varargin)
-            self.hFigure = figure('Name', 'Perspective Simulator',...
-                                  ...'Units', 'normalized',...
-                                  ...'Position', [0.0 0.0 0.75 0.75],...
-                                  'Visible', 'off',...
-                                  'defaultSurfaceEdgeColor', 'none',...
-                                  'defaultPatchEdgeColor', 'none');
+            self.hFigure = clf(figure(1));
+            
+            set(self.hFigure, 'Name', 'Perspective Simulator',...
+                              ...'Units', 'normalized',...
+                              ...'Position', [0.0 0.0 0.75 0.75],...
+                              'Visible', 'off',...
+                              'defaultSurfaceEdgeColor', 'none',...
+                              'defaultPatchEdgeColor', 'none');
             
             self.MakeWindow();
             
@@ -50,6 +50,7 @@ classdef Simulator < handle
             self.CreateScene()
             self.CreateStaticObjects();
             self.CreateMovingObjects();
+            self.CreateLighting();
             
             %% Set the initial camara pos
             posvec = [0 self.RoadParams.laneWidth/2 self.CameraParams.height];
@@ -62,7 +63,8 @@ classdef Simulator < handle
 %                 % Why do we need to do this???? And it still doesn't fully work
 %                 set(gca, 'xlim', [-10 500], 'ylim', [-10000 10000], 'zlim', [-10000, 10000]);   
             else
-                camva(45)
+%                 camva(45)
+                camva(60)
             end
 
             %% Make the GUI ready for viewing
@@ -76,11 +78,20 @@ classdef Simulator < handle
         function MakeWindow(self)
             self.HD.MainView = axes('Parent', self.hFigure');
             
+%             imsize = [1 1 512 512];
+            imsize = [1 1 640 480];
+%             imsize = [1 1 800 600];
+%             imsize = [1 1 1024 768];
+%             imsize = [1 1 1280 960];
+            
             set(self.HD.MainView,...
                 'Color', 'c',...
                 'ydir', 'reverse',...    % this it to make it NED-ish, we'll treat Z as up
                 'Units', 'pixels',....
-                'Position', [1 1 512 512]); % This make it the desired image size
+                ...'PlotBoxAspectRatio', [1 1 1],...
+                'Position', imsize); % This make it the desired image size
+            set(self.hFigure, 'Units', 'pixels',...
+                'Position', imsize)
             
             axis(self.HD.MainView, 'image');
             axis(self.HD.MainView, 'off');
@@ -104,17 +115,37 @@ classdef Simulator < handle
             %% Draw the roadway
             [x,y,z] = self.Road(self.SimulationDistance, self.RoadParams);
             self.SceneObjects.Road = patch(x, y, z, self.RoadParams.roadColor);
+            self.SceneObjects.OpposingRoad = patch(x, -y, z, self.RoadParams.roadColor);
             
             %% Draw the lane markings
-            [x,y,z] = self.LaneMarkings(self.SimulationDistance, self.RoadParams);
-            for n = 0:self.RoadParams.numLanes
-                self.SceneObjects.LaneMarkings(n+1) = patch(x, y + n*self.RoadParams.laneWidth - self.RoadParams.laneMarkerWidth/2, z, self.RoadParams.laneColor);
+            % Double yellow centerline Markings
+            [x,y,z] = self.SolidLaneMarkings(self.SimulationDistance, self.RoadParams);
+            self.SceneObjects.LaneMarkings(1) = patch(x, y+.25*self.RoadParams.laneMarkerWidth, z, [1 1 0]);
+            self.SceneObjects.LaneMarkings(end+1) = patch(x, -y-.25*self.RoadParams.laneMarkerWidth, z, [1 1 0]);
+            
+            % Dashed white lines
+            [x,y,z] = self.DashedLaneMarkings(self.SimulationDistance, self.RoadParams);
+            for n = 1:(self.RoadParams.numLanes-1)
+                self.SceneObjects.LaneMarkings(end+1) = patch(x, y + n*self.RoadParams.laneWidth - self.RoadParams.laneMarkerWidth/2, z, self.RoadParams.laneColor);
+                self.SceneObjects.LaneMarkings(end+1) = patch(x, -(y + n*self.RoadParams.laneWidth - self.RoadParams.laneMarkerWidth/2), z, self.RoadParams.laneColor);
             end
+            
+            % Solid white edge lines
+            [x,y,z] = self.SolidLaneMarkings(self.SimulationDistance, self.RoadParams);
+            self.SceneObjects.LaneMarkings(end+1) = patch(x, y + (n+1)*self.RoadParams.laneWidth - self.RoadParams.laneMarkerWidth/2, z, self.RoadParams.laneColor);
+            self.SceneObjects.LaneMarkings(end+1) = patch(x, -(y + (n+1)*self.RoadParams.laneWidth - self.RoadParams.laneMarkerWidth/2), z, self.RoadParams.laneColor);
+            
         end
         
         function CreateStaticObjects(self)
-            
-            
+            %% Create a row of trees alongside the road
+            y = (self.RoadParams.numLanes + 0.5) * self.RoadParams.laneWidth + 4;
+            treeSeperation = 100; %m
+            numTrees = floor(self.SimulationDistance/treeSeperation);      % Place a tree every 10 meters
+            for n = 1:numTrees
+                self.StaticObjects.Trees(n) = self.Tree(n*treeSeperation, y);
+                self.StaticObjects.Trees(n+numTrees) = self.Tree(n*treeSeperation, -y);
+            end
         end
         
         function CreateMovingObjects(self)
@@ -127,13 +158,19 @@ classdef Simulator < handle
             end
             
         end
+        
+        function CreateLighting(self)
+            for n = 0:500:self.SimulationDistance
+                light('Position', [n, 0 -2000])
+            end
+        end
     end
     
     methods
         function Simulate(self)
             
-            mov = VideoWriter('temp.avi');
-            open(mov);
+%             mov = VideoWriter('temp.avi');
+%             open(mov);
             
             % For 25m x #cars, take 1m steps
             for n = 1:25*(self.SimulationParams.numCars+1)
@@ -141,16 +178,16 @@ classdef Simulator < handle
                     break
                 end
                 posvec = [n self.RoadParams.laneWidth*sin(n/25) + 3*self.RoadParams.laneWidth/2 self.CameraParams.height];
-                targvec = posvec + self.DriverParams.lookAheadDistance*[1 self.RoadParams.laneWidth/25*cos(n/25) 0];
+                targvec = posvec + self.DriverParams.lookAheadDistance*[1 0*self.RoadParams.laneWidth/25*cos(n/25) 0];
                 campos(posvec);
                 camtarget(targvec);
 
-                im = getframe(self.HD.MainView);
+%                 im = getframe(self.HD.MainView);
                 
-                writeVideo(mov, im.cdata);
-%                 pause(0.1), drawnow
+%                 writeVideo(mov, im.cdata);
+                pause(0.1), drawnow
             end
-            close(mov)
+%             close(mov)
         end
     end
     
@@ -168,23 +205,95 @@ classdef Simulator < handle
         end
         
         function [x,y,z] = Road(length, RoadParams)
-            width = RoadParams.numLanes * RoadParams.laneWidth;
+            width = (RoadParams.numLanes + 0.5) * RoadParams.laneWidth;
             x = [0 0 length length];
             y = [0 width width 0];
             z = [0 0 0 0] + 0.01;
         end
         
-        function [x,y,z] = LaneMarkings(length, RoadParams)
+        function [x,y,z] = DoubleLaneMarkings(length, RoadParams)
+            width = RoadParams.laneMarkerWidth;
+            x = [0 0 length length 0 0 0 length length 0];
+            y = width*[-0.25 -1.25 -1.25 -0.25 -0.25 0.25 1.25 1.25 0.25 0.25];
+            z = [0 0 0 0 0 0 0 0 0 0] + 0.02;
+            
+%             width = RoadParams.laneMarkerWidth;
+%             x = [0 0 length length];
+%             y = [0 width width 0];
+%             z = [0 0 0 0] + 0.02;
+        end
+        
+        function [x,y,z] = SolidLaneMarkings(length, RoadParams)
             width = RoadParams.laneMarkerWidth;
             x = [0 0 length length];
             y = [0 width width 0];
             z = [0 0 0 0] + 0.02;
         end
         
+        function [x,y,z] = DashedLaneMarkings(length, RoadParams)
+            % On rural highways, broken lines should consist of 3 m (10 ft)
+            % line segments and 9 m (30 ft) gaps, or similar dimensions in
+            % a similar ratio of line segments to gaps as appropriate for
+            % traffic speeds and need for delineation
+            %
+            % Option: A dotted line may consist of 0.6m (2ft) line
+            % segments, and 1.2m (4ft) or longer gaps, with a maximum
+            % segment to gap ratio of 1-to-3 
+            % Ref. http://mutcd.fhwa.dot.gov/pdfs/millennium/06.14.01/3ndi.pdf
+            % page 10
+            
+            width = RoadParams.laneMarkerWidth;
+            segLen = 3;
+            origx = [0 0 segLen segLen];
+            origy = [0 width width 0];
+            origz = [0 0 0 0] + 0.02;
+            
+            seperation = segLen + segLen*3;
+            numSegments = floor(length/(seperation)); 
+            x = [];
+            for n = 1:numSegments
+                x = [x origx+n*seperation];
+            end
+            y = repmat(origy,1,numSegments);
+            z = repmat(origz,1,numSegments);
+        end
+        
         function [x,y,z] = Car(CarParams)
             x = CarParams.length*[0.5 0.5 0.5 0.5 0.5; 0 0 1 1 0; 0 0 1 1 0; 0.5 0.5 0.5 0.5 0.5];
             y = CarParams.width *[0.5 0.5 0.5 0.5 0.5; 0 1 1 0 0; 0 1 1 0 0; 0.5 0.5 0.5 0.5 0.5];
             z = CarParams.height*[0 0 0 0 0; 0 0 0 0 0; 1 1 1 1 1; 1 1 1 1 1] + 3*0.01;
+        end
+        
+        function h = Tree(x,y)
+            ang = linspace(0,2*pi,10);
+            bx = cos(ang);
+            by = sin(ang);
+            bz = ones(size(ang));
+            trunkx = [0*bx;
+                      0.3048*bx;    % 1ft
+                      0.3048*bx;    % 1ft
+                      0*bx];
+            trunky = [0*by;
+                      0.3048*by;    % 1ft
+                      0.3048*by;    % 1ft
+                      0*by];
+            trunkz = [0*bz;
+                      0*bz;         
+                      0.9144*bz;    % 3ft
+                      0.9144*bz];   % 3ft
+                
+            topx = [0*bx;
+                    1.8288*bx;    % 6ft
+                    0*bx];
+            topy = [0*by;
+                    1.8288*by;    % 6ft
+                    0*by];
+            topz = [0.9144*bz;   % 3ft
+                    0.9144*bz;   % 3ft      
+                    4.572*bz];   % 15ft
+                  
+            h.trunk = surface(trunkx + x, trunky + y, trunkz, 'FaceColor', [92 64 51]/255);
+            h.top = surface(topx + x, topy + y, topz, 'FaceColor', [25 50 25]/255);
         end
     end
     
